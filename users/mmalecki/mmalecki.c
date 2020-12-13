32dcd440ecb2c16e06c5259808b32a3bb203a05d
Maciej Małecki <me@mmalecki.com>
2020-12-13 18:13:34 +0100

    Move out the macro processing to a userspace function

---
+++ users/mmalecki/mmalecki.c
@@ -0,0 +1,24 @@
+#include QMK_KEYBOARD_H
+#include "mmalecki.h"
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case TERRAFORM:
+      if (record->event.pressed) {
+        SEND_STRING("terraform ");
+
+      }
+      break;
+    case GIT:
+      if (record->event.pressed) {
+        SEND_STRING("git ");
+      }
+      break;
+    case GIT_COMMIT_MSG:
+      if (record->event.pressed) {
+        SEND_STRING("git commit -m ''"SS_TAP(X_LEFT));
+      }
+      break;
+  }
+  return true;
+}
---
+++ users/mmalecki/mmalecki.h
@@ -0,0 +1,5 @@
+enum userspace_custom_keycodes {
+  GIT,
+  GIT_COMMIT_MSG,
+  TERRAFORM,
+};
