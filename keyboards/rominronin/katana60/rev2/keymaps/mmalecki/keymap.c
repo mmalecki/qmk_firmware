a4a73ab644d15c45dcfce56ad51f58ece2b5d433
Maciej Małecki <me@mmalecki.com>
2020-12-18 08:19:50 +0100

    Left space is modtap shift

--- keyboards/rominronin/katana60/rev2/keymaps/mmalecki/keymap.c
+++ keyboards/rominronin/katana60/rev2/keymaps/mmalecki/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,    KC_LBRC,   KC_RBRC,    KC_Y,    KC_U,    KC_I, KC_O,    KC_P,    KC_BSPC,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,    KC_BSLASH, KC_QUOTE,   KC_H,    KC_J,    KC_K, KC_L,    KC_SCLN, KC_ENTER,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_PGUP,   KC_DEL,     KC_PGDN, KC_N,    KC_M, KC_COMM, KC_DOT,  KC_SLSH, MO(FN),
-    KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, KC_SPACE, KC_ENT,  KC_SPACE,  KC_RALT,    KC_RGUI, MO(FN)
+    KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, MT(MOD_LSFT, KC_SPACE), KC_ENT, KC_SPACE, KC_RALT, KC_RGUI, MO(FN)
   ),
   [FN] = LAYOUT_1_c(
     _______, _______, _______, _______, _______,  _______, _______,    _______,   _______, _______, _______, _______, _______,  _______, _______,
