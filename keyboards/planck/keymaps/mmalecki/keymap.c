c5ed612aa8070ed77b9dc4081b3560d727d26558
Maciej Małecki <me@mmalecki.com>
2020-12-18 08:17:18 +0100

    Add modtap for space, tap dance for brackets

--- keyboards/planck/keymaps/mmalecki/keymap.c
+++ keyboards/planck/keymaps/mmalecki/keymap.c
@@ -30,6 +30,17 @@ enum planck_layers {
   _LAYER4,
 };
 
+enum tap_dances {
+  TD_LBRACKETS,
+  TD_RBRACKETS
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_LBRACKETS] = ACTION_TAP_DANCE_DOUBLE(KC_LBRACKET, KC_LPRN),
+  [TD_RBRACKETS] = ACTION_TAP_DANCE_DOUBLE(KC_RBRACKET, KC_RPRN)
+};
+
+
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 
@@ -38,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESCAPE,      KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSPACE,
     KC_TAB,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
     KC_LCTRL,       KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_ENTER,
-    KC_LSHIFT,      KC_LBRACKET,    KC_LGUI,        KC_LALT,        LOWER,          KC_SPACE,       KC_NO,          RAISE,          KC_RALT,        KC_RGUI,        KC_RBRACKET,    MO(4)
+    KC_LSHIFT,      TD(TD_LBRACKETS), KC_LGUI,      KC_LALT,        LOWER,          MT(MOD_LSFT, KC_SPACE), KC_NO,  RAISE,          KC_RALT,        KC_RGUI,        TD(TD_RBRACKETS), MO(4)
   ),
 
   [_LOWER] = LAYOUT_planck_grid(
--- keyboards/planck/keymaps/mmalecki/rules.mk
+++ keyboards/planck/keymaps/mmalecki/rules.mk
@@ -2,5 +2,9 @@ SRC += muse.c mmalecki.c
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 CONSOLE_ENABLE = no
+AUDIO_ENABLE = no
 COMMAND_ENABLE = no
 MOUSEKEY_ENABLE = no
+TAP_DANCE_ENABLE = yes
+RGB_MATRIX_ENABLE = yes
+
