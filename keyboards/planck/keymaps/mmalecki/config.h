b67699712da35445afcdd7180a773bda70768ef8
Maciej Małecki <me@mmalecki.com>
2020-12-18 08:16:09 +0100

    No longer Onyx Configurator

--- keyboards/planck/keymaps/mmalecki/config.h
+++ keyboards/planck/keymaps/mmalecki/config.h
@@ -8,9 +8,4 @@
 
 #define ENCODER_RESOLUTION 4
 
-/*
-  Set any config.h overrides for your specific keymap here.
-  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
-*/
-#define ORYX_CONFIGURATOR
 #define RGB_MATRIX_STARTUP_SPD 60
