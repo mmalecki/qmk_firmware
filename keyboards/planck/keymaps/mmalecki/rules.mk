93ceba4678b2e98d3e30c8f97386df4bbfbd0cbd
Maciej Małecki <me@mmalecki.com>
2020-12-19 15:00:12 +0100

    Reenable audio

--- keyboards/planck/keymaps/mmalecki/rules.mk
+++ keyboards/planck/keymaps/mmalecki/rules.mk
@@ -2,7 +2,7 @@ SRC += muse.c mmalecki.c
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 CONSOLE_ENABLE = no
-AUDIO_ENABLE = no
+AUDIO_ENABLE = yes
 COMMAND_ENABLE = no
 MOUSEKEY_ENABLE = no
 TAP_DANCE_ENABLE = yes
