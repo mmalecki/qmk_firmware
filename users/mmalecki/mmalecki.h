c28348a090d5e1dde89a45c35ec97f771a189f63
Maciej Małecki <me@mmalecki.com>
2020-12-19 15:16:55 +0100

    Respect other safe ranges

--- users/mmalecki/mmalecki.h
+++ users/mmalecki/mmalecki.h
@@ -1,5 +1,11 @@
+#ifdef KEYBOARD_planck_ez
+  #define _SAFE_RANGE EZ_SAFE_RANGE
+#else
+  #define _SAFE_RANGE SAFE_RANGE
+#endif
+
 enum userspace_custom_keycodes {
-  GIT = EZ_SAFE_RANGE,
+  GIT = _SAFE_RANGE,
   GIT_COMMIT_MSG,
   TERRAFORM,
 };
