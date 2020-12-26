54af67d6a5643955380d17d4d53325f0fb7ac909
Maciej Małecki <me@mmalecki.com>
2020-12-26 23:58:28 +0100

    Use the new progress stuff

--- keyboards/planck/keymaps/mmalecki/rules.mk
+++ keyboards/planck/keymaps/mmalecki/rules.mk
@@ -1,4 +1,4 @@
-SRC += muse.c mmalecki.c
+SRC += muse.c mmalecki.c progress.c
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 CONSOLE_ENABLE = no
@@ -7,4 +7,7 @@ COMMAND_ENABLE = no
 MOUSEKEY_ENABLE = no
 TAP_DANCE_ENABLE = yes
 RGB_MATRIX_ENABLE = yes
+RAW_ENABLE = yes
 
+# I make my own rules here
+PROGRESS_ENABLE = yes
