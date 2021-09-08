0096037529bfe0f53dcecc3e5bf1f9a50239c2d5
Maciej Małecki <me@mmalecki.com>
2021-09-08 16:41:58 +0200

    Disable some stray keys on Boardwalk

--- keyboards/boardwalk/keymaps/mmalecki/keymap.c
+++ keyboards/boardwalk/keymaps/mmalecki/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,   KC_RBRC,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_BSLASH, KC_QUOTE, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_HOME,   KC_END,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-      KC_LCTL, MO(_FN), KC_LGUI, KC_LALT, LOWER,   MT_LSFT_SPC,        MT_LSFT_SPC,       RAISE,   KC_RALT, KC_UP,   KC_RGHT, MO(_FN)
+      KC_LCTL, MO(_FN), KC_LGUI, KC_LALT, LOWER,   MT_LSFT_SPC,        MT_LSFT_SPC,       RAISE,   KC_RALT, KC_NO,   KC_NO,   MO(_FN)
   ),
 
   [_LOWER] = EXPAND(
